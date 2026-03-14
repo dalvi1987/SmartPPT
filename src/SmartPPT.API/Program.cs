57090309cd1fefee6bd081783dcadc47fb2fb293
Shekhar Dalvi <Shekhar.dalvi@outlook.com>
2026-03-14 17:34:59 +0530

    presentation di

--- src/SmartPPT.API/Program.cs
+++ src/SmartPPT.API/Program.cs
@@ -1,12 +1,14 @@
 using SmartPPT.Template.Infrastructure.DependencyInjection;
 using SmartPPT.SlideEngine.Infrastructure.DependencyInjection;
 using SmartPPT.Agent.Infrastructure.DependencyInjection;
+using SmartPPT.Presentation.Infrastructure.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddTemplateModule();
 builder.Services.AddSlideEngineModule();
 builder.Services.AddAgentModule();
+builder.Services.AddPresentationModule();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
